761e5b64619f020f2d8791560cdfc95debeed43b
gena03111975 <gepota1@tut.by>
2024-11-04 21:44:33 +0300

    добавили Door.java

---
+++ Door.java
@@ -0,0 +1,21 @@
+public class Door {
+    private boolean isPrize;
+
+    public Door(boolean isPrize) {
+        this.isPrize = isPrize;
+    }
+
+    public boolean isPrize() {
+        return isPrize;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Door{" +
+                "isPrize=" + isPrize +
+                '}';
+    }
+
+
+}
